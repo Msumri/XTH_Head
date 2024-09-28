a8ba4297dfc960c1bdb29a64877dfaf8fb9f9229
mohammed al harthy <malhart28@gmail.com>
2024-09-27 23:44:53 -0500

    updated app.py to allow login and admin prevlages

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
-from flask import Flask, render_template,request,session
-from flask_socketio import SocketIO, send, emit
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+from functools import wraps
 
 
 
 app = Flask(__name__)
 app.secret_key = 'hakoonamatata'  
-socketio = SocketIO(app)
 
 #hard code ids --its a bad idea but just for this simulation its fine
 agentsid=['6218','7154','9819','5287','8030']
 agentpass=['1899','3729','6515','9364','9484']
+
+
+# Custom decorator to require login
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('login'):
+            return redirect(url_for('login_page'))  # Redirect to login if not logged in
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @app.route('/')
+@login_required
 def home():
     return render_template('index.html')
 
-@app.route('/map', methods=['POST','GET'])
-def map():
+@app.route('/login', methods=['POST','GET'])
+def login_page():
     agentid=None
     password=None
-    client_ip = request.remote_addr
+    login = session.get('login', False)
     if request.method == 'POST':
         try:
             agentid=request.form['agens']
@@ -27,30 +39,24 @@ def map():
             
             if agentid in agentsid:
                 agentindex=agentsid.index(agentid)
-                print("TTTTT")
                 if password in agentpass and agentpass.index(password)==agentindex:
                     session['login'] = True
+                    return redirect(url_for('home'))
+                else:
+                    flash(f'Incorrect password for Agent ID# {agentid}')
+
             else :
                 session['login'] =False
+                flash('This user doesnt exist')
+        except:
+            login = session.get('login', False)
+    else:
+        if login:
+            return redirect(url_for('home'))
+            
+    return render_template('loginpage.html',login=login)
 
-        finally:
-            login=session['login']
-            print(agentid)
-            print(type(agentid))
-            print(password)
-            print(type(password))
-    login = session.get('login', False)
-    return render_template('map.html',login=login,client_ip=client_ip)
 
-
-@app.route('/chatapp')
-def chatapp():
-    return render_template('intsim.html')
-@socketio.on('send_message')
-def handle_message(data):
-    print(f"Received message: {data['msg']} from {data['username']}")
-    # Broadcast message to all connected clients
-    emit('message', data, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app,debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
