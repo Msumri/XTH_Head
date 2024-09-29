49dcf1a3b3adbccd4194db9a62e634c1ffb27c7f
mohammed al harthy <malhart28@gmail.com>
2024-09-29 14:30:17 -0500

    added a sperate loging page and agent loging page to make it more challenging

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app.secret_key = 'hakoonamatata'
 #hard code ids --its a bad idea but just for this simulation its fine
 agentsid=['6218','7154','9819','5287','8030']
 agentpass=['1899','3729','6515','9364','9484']
-
+usernames=['midoshy','mohammed.alharthy@crosstownhigh.org']
+passwords='022695'
 
 # Custom decorator to require login
 def login_required(f):
@@ -37,17 +38,16 @@ def login_page():
             password=request.form['password']
         
             
-            if agentid in agentsid:
-                agentindex=agentsid.index(agentid)
-                if password in agentpass and agentpass.index(password)==agentindex:
+            if agentid in usernames:
+                if password == passwords :
                     session['login'] = True
-                    return redirect(url_for('home'))
+                    return redirect(url_for('home'))                    
                 else:
-                    flash(f'Incorrect password for Agent ID# {agentid}')
+                    flash('check your user or password')
 
-            else :
-                session['login'] =False
+            else:
                 flash('This user doesnt exist')
+
         except:
             login = session.get('login', False)
     else:
@@ -60,6 +60,37 @@ def login_page():
 def logout():
     session.pop('login', None)  # Remove 'login' from session
     return redirect(url_for('login_page'))  # Redirect to login page after logging out
+
+
+### agnet login info####
+
+@app.route('/agent_login', methods=['POST','GET'])
+def agent_page():
+    agentid=None
+    password=None
+    login = session.get('agent_login', False)
+    if request.method == 'POST':
+        try:
+            agentid=request.form['agens']
+            password=request.form['password']
+        
+            
+            if agentid in agentsid:
+                agentindex=agentsid.index(agentid)
+                if password in agentpass and agentpass.index(password)==agentindex:
+                    session['agent_login'] = True
+                    return redirect(url_for('home'))
+                else:
+                    flash(f'Incorrect password for Agent ID# {agentid}')
+
+            else :
+                session['agent_login'] =False
+                flash('This user doesnt exist')
+        except:
+            login = session.get('agent_login', False)
+    
+            
+    return render_template('Agentlogin.html',login=login)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
